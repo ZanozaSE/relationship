ce6ca78a5ba7429ebc836510da49e5285a3d3c60
Артём Кузьмин <111683856+ZanozaSE@users.noreply.github.com>
2026-09-02 22:15:38 +0300

    Add metric importance API route

--- metrics/urls.py
+++ metrics/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import MyMetricsView
+from .views import MetricImportanceView, MyMetricsView
 
 
 urlpatterns = [
     path('', MyMetricsView.as_view(), name='my_metrics'),
+    path(
+        '<int:metric_id>/importance/',
+        MetricImportanceView.as_view(),
+        name='metric_importance',
+    ),
 ]
