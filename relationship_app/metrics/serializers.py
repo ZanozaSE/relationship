2f5f7cfbe501457147bff44488a2f0a516090a29
Артём Кузьмин <111683856+ZanozaSE@users.noreply.github.com>
2026-09-03 23:50:53 +0300

    Исправить запуск backend после переноса фильтрации

--- relationship_app/metrics/serializers.py
+++ relationship_app/metrics/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from .models import CoupleMetric, MetricImportance, MetricValue
-from .services import get_latest_effective_metric_value
+from .services import get_latest_metric_value
 
 
 class CoupleMetricSerializer(serializers.ModelSerializer):
@@ -33,12 +33,12 @@ class CoupleMetricSerializer(serializers.ModelSerializer):
 
     def get_latest_value(self, obj):
         user = self.context['request'].user
-        metric_value = get_latest_effective_metric_value(obj, user)
+        metric_value = get_latest_metric_value(obj, user)
         return metric_value.value if metric_value else None
 
     def get_latest_satisfaction(self, obj):
         user = self.context['request'].user
-        metric_value = get_latest_effective_metric_value(obj, user)
+        metric_value = get_latest_metric_value(obj, user)
         return metric_value.satisfaction if metric_value else None
 
 
