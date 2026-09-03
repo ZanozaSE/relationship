0f26ba8c50ade85884d747be2288a56e6de40d64
Артём Кузьмин <111683856+ZanozaSE@users.noreply.github.com>
2026-09-03 12:49:43 +0300

    Добавлены маршруты целей и заметок

--- relationship_app/couples/urls.py
+++ relationship_app/couples/urls.py
@@ -2,27 +2,21 @@ from django.urls import path
 
 from .views import (
     CreateCoupleView,
+    CoupleGoalDetailView,
+    CoupleGoalsView,
+    CoupleNoteDetailView,
+    CoupleNotesView,
     JoinCoupleView,
     MyCoupleView,
 )
 
 
 urlpatterns = [
-    path(
-        '',
-        CreateCoupleView.as_view(),
-        name='create-couple',
-    ),
-
-    path(
-        'join/',
-        JoinCoupleView.as_view(),
-        name='join-couple',
-    ),
-
-    path(
-        'me/',
-        MyCoupleView.as_view(),
-        name='my-couple',
-    ),
+    path('', CreateCoupleView.as_view(), name='create-couple'),
+    path('join/', JoinCoupleView.as_view(), name='join-couple'),
+    path('me/', MyCoupleView.as_view(), name='my-couple'),
+    path('goals/', CoupleGoalsView.as_view(), name='couple-goals'),
+    path('goals/<int:goal_id>/', CoupleGoalDetailView.as_view(), name='couple-goal-detail'),
+    path('notes/', CoupleNotesView.as_view(), name='couple-notes'),
+    path('notes/<int:note_id>/', CoupleNoteDetailView.as_view(), name='couple-note-detail'),
 ]
