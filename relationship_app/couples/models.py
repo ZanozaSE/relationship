a2317e69d883bc476b4622fa02a20972c9a7a140
Артём Кузьмин <111683856+ZanozaSE@users.noreply.github.com>
2026-09-03 17:23:16 +0300

    Добавить длительность отношений

--- relationship_app/couples/models.py
+++ relationship_app/couples/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Couple(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+    together_days = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"Couple #{self.id}"
