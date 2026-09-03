61c4522e3c862c38ba16607a186ecb5785dec647
Артём Кузьмин <111683856+ZanozaSE@users.noreply.github.com>
2026-09-03 16:28:07 +0300

    Добавить маршрут выхода из пары

--- relationship_app/couples/urls.py
+++ relationship_app/couples/urls.py
@@ -8,6 +8,7 @@ from .views import (
     CoupleNoteDetailView,
     CoupleNotesView,
     JoinCoupleView,
+    LeaveCoupleView,
     MyCoupleView,
 )
 
@@ -16,6 +17,7 @@ urlpatterns = [
     path('', CreateCoupleView.as_view(), name='create-couple'),
     path('join/', JoinCoupleView.as_view(), name='join-couple'),
     path('me/', MyCoupleView.as_view(), name='my-couple'),
+    path('leave/', LeaveCoupleView.as_view(), name='leave-couple'),
     path('invitation/', CoupleInvitationView.as_view(), name='couple-invitation'),
     path('goals/', CoupleGoalsView.as_view(), name='couple-goals'),
     path('goals/<int:goal_id>/', CoupleGoalDetailView.as_view(), name='couple-goal-detail'),
