c52ffb90eba0960c7b9b3478ca2abe26519a7f3c
Артём Кузьмин <111683856+ZanozaSE@users.noreply.github.com>
2026-09-04 00:55:05 +0300

    Добавить API endpoints восстановления пароля

--- relationship_app/users/urls.py
+++ relationship_app/users/urls.py
@@ -4,7 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import ProfileUpdateView, RegisterView, MeView
+from .views import (
+    MeView,
+    PasswordResetConfirmView,
+    PasswordResetRequestView,
+    ProfileUpdateView,
+    RegisterView,
+)
 
 
 urlpatterns = [
@@ -13,28 +19,34 @@ urlpatterns = [
         RegisterView.as_view(),
         name='register',
     ),
-
     path(
         'login/',
         TokenObtainPairView.as_view(),
         name='login',
     ),
-
     path(
         'token/refresh/',
         TokenRefreshView.as_view(),
         name='token_refresh',
     ),
-
     path(
         'me/',
         MeView.as_view(),
         name='me',
     ),
-
     path(
         'profile/',
         ProfileUpdateView.as_view(),
         name='profile-update',
+    ),
+    path(
+        'password-reset/',
+        PasswordResetRequestView.as_view(),
+        name='password-reset',
+    ),
+    path(
+        'password-reset/confirm/',
+        PasswordResetConfirmView.as_view(),
+        name='password-reset-confirm',
     ),
 ]
